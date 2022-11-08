2d4eb55523c1565a16613605d1fed9c41c0bcb7a
TB <tatianabertazoli@gmail.com>
2022-11-07 18:05:36 -0600

    added security and authentication

--- models/User.js
+++ models/User.js
@@ -1,22 +1,24 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model { }
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 // define table columns and configuration
 User.init(
     {
         // define an id column
         id: {
-            // use the special Sequelize DataTypes object provide what type of data it is
-            type: DataTypes.INTEGER,
-            // this is the equivalent of SQL's `NOT NULL` option
-            allowNull: false,
-            // instruct that this is the Primary Key
-            primaryKey: true,
-            // turn on auto increment
-            autoIncrement: true
+            type: DataTypes.INTEGER, // use the special Sequelize DataTypes object provide what type of data it is
+            allowNull: false, // this is the equivalent of SQL's `NOT NULL` option
+            primaryKey: true, // instruct that this is the Primary Key
+            autoIncrement: true  // turn on auto increment
         },
         // define a username column
         username: {
@@ -45,6 +47,18 @@ User.init(
         }
     },
     {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            // set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            },
+        },
         // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
 
         // pass in our imported sequelize connection (the direct connection to our database)
@@ -56,8 +70,9 @@ User.init(
         // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
         underscored: true,
         // make it so our model name stays lowercase in the database
-        modelName: 'user'
+        modelName: 'user',
     }
+
 );
 
 
